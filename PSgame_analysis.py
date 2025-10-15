b57adec1ec32024843a029df6f4ba029c1bbf356
a1issa369 <146799085+a1issa369@users.noreply.github.com>
2025-10-15 16:17:15 -0700

    Add GameAnalysis class for PlayStation classifier

---
+++ PSgame_analysis.py
@@ -0,0 +1,11 @@
+# game_analysis.py
+import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, classification_report
+import numpy as np
+
+class GameAnalysis:
+    """Analysis tools for the PlayStation classifier"""
+    
+    
